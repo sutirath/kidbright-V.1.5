e3ba00abe1d6d71668f8374dd9232006941413ee
sutirath <39222262+sutirath@users.noreply.github.com>
2020-05-14 13:19:39 +0700

    update

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  name: "kidbright-arduino -test",
+  name: "kidbright-arduino 1.5",
   platform: "arduino-esp32",
-  title: "Kidbright-test",
+  title: "kidbright-arduino 1.5",
   description: "บอร์ด KidBright เขียนบนแพลตฟอร์ม Arduino-ESP32 ทำให้สามารถใช้ความสามารถใหม่ ๆ และปลั๊กอินของ Arduino ได้",
   author: "Maker Asia (maintain by Comdet)",
-  website: "https://github.com/MakerAsia/kidbright-arduino-board/",
+  website: "https://github.com/sutirath/kidbright-V.1.5/",
   email: "makerasia@gmail.com",
-  git: "https://github.com/MakerAsia/kidbright-arduino-board/",
+  git: "https://github.com/sutirath/kidbright-V.1.5/",
   image: "/static/display-test.jpg",
   version: "1.5.0",
 };
